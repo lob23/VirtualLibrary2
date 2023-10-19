173712255840bca8478d3d56decfc1108c5afed4
Nguyen Phuc Bao Uyen <hollyuyensan@gmail.com>
2023-10-19 08:41:09 +0700

    init main.js

---
+++ app/main.js
@@ -0,0 +1,10 @@
+"use strict"
+
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const hbs = require("express-handlebars").create(defaultLayout: "main");
+
+
