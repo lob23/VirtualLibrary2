d2c2068cc09312d339078c8ba090d6141a4c0e33
lob23 <Hvblong21@apcs.fitus.edu.vn>
2023-10-19 11:04:56 +0700

    App skeleton
    App basic structure and installation of nodemon, dotenv, tailwind, handle-bars

--- app/main.js
+++ app/main.js
@@ -5,6 +5,21 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const hbs = require("express-handlebars").create(defaultLayout: "main");
+const hbs = require("express-handlebars").create({defaultLayout: "main"});
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("port", 3000|process.env.PORT);
 
+app.get("/", (req, res) => {
+    res.render("home_screen", {
+        accessing_date: new Date()
+    });
+})
 
+app.use((req, res) =>{
+    res.send("404 NOT FOUND ERROR");
+})
+
+app.listen(app.get("port"), () => {
+    console.log("Application is listening on port " + app.get("port") + ", press CTR + C to terminate the listening.")
+});
