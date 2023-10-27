bc85c92bd72003cd4e527db212e6d1764d2fd7f8
lob23 <hvblong21@apcs.fitus.edu.vn>
2023-10-27 15:47:27 +0700

    file modifications
    The project should follow the MVC architecture. This commit created and separated the needed files/respiratories

---
+++ app/src/config/appConfig.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("express-handlebars").create({defaultLayout: "main"});
+
+const config = (app) => {
+    // console.log(path.join("./app", "views"));
+    app.engine("handlebars", hbs.engine);
+    app.set("views", path.join("./src", "views"));
+    app.set("view engine", "handlebars");
+    app.set("port", 3000|process.env.PORT);
+    app.use(express.static(path.join("./src", "public")));
+};
+
+module.exports = config;
