36ed6dca1e3e83c9da860823b2743417965b23f4
phuccom000 <nhphuc21@apcs.fitus.edu.vn>
2023-11-30 19:15:14 +0700

    search wip

---
+++ app/src/app/search/page.js
@@ -0,0 +1,12 @@
+import React from 'react';  // Import React
+import _searchForm from "@/app/search/search_form"
+
+
+export default function search_screen(){
+    return (
+        <div>
+        
+            <_searchForm />
+        </div>
+    );
+}
