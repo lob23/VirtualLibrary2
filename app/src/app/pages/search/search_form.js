4e1320068fd38359132432ffe0b743a8f4325826
phuccom000 <nhphuc21@apcs.fitus.edu.vn>
2023-12-16 15:31:54 +0700

    Update search_form.js

--- app/src/app/pages/search/search_form.js
+++ app/src/app/pages/search/search_form.js
@@ -13,7 +13,7 @@ export default function search_form() {
   return (
     <>
       <div class="relative w-screen h-screen bg-cream">
-        <div class="z-40 mx-40 my-40 rounded-md flex items-center h-12 rounded-lg bg-white overflow-hidden shadow-md shadow-blue-500/50">
+        <div class="relative z-40 mx-40 my-40 rounded-md flex items-center h-12 rounded-lg bg-white overflow-hidden shadow-md shadow-blue-500/50">
               <div class="grid place-items-center h-full w-12 text-gray-300">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -21,7 +21,7 @@ export default function search_form() {
               </div>
       
               <input
-              class="h-full w-full text-sm text-gray-700 border-0 focus:border-0 shadow-none py-20 placeholder:text-blue"
+              class="h-full w-full text-sm text-gray-700 border-0 focus:border-0 shadow-none py-20 placeholder:text-blue placeholder:text-opacity-50"
               type="text"
               id="search"
               placeholder="Enter a book" /> 
@@ -31,20 +31,20 @@ export default function search_form() {
               Search
               </button>
           </div>
-        <div class="z-10">
-          <div class="absolute -top-20 -right-20  w-[300px] h-[300px]">
+          <div class="z-10">
+          <div class="absolute -top-[-50px] -right-[20px]  w-[250px] h-[250px]">
               <img
                 class="object-contain w-full h-full  "
                 src="/image/reg_cir.png"
               ></img>
             </div>
-          <div class="absolute top-[20px] left-[120px]  w-[120px] h-[150px]">
+          <div class="absolute top-[20px] left-[-120px]  w-[300px] h-[300px]">
               <img
                 class="object-contain w-full h-full "
                 src="/image/reg_cir.png"
               ></img>
             </div>
-          <div class="absolute top-[60px] right-[120px]  w-[80px] h-[80px]">
+          <div class="absolute top-[120px] left-[120px]  w-[150px] h-[150px]">
               <img
                 class="object-contain w-full h-full "
                 src="/image/reg_cir.png"
