4e2f18ed8e7bd4a79a40b1681e4dbb05b2497f6a
tttrinhtran <89054365+tttrinhtran@users.noreply.github.com>
2023-12-26 14:17:08 +0700

    done front end landing page

--- app/src/app/pages/landing/landing_page.js
+++ app/src/app/pages/landing/landing_page.js
@@ -33,7 +33,7 @@ export default function landing(){
       </div>
     
       <div className="relative w-full h-full overflow-y-auto no-scrollbar">
-        <div className="grid grid-flow-col grid-cols-3 relative w-screen h-screen">
+        <div className="grid grid-flow-col grid-cols-2 relative w-screen h-screen">
           <div className="relative col-span-1 flex flex-col flex-col-3 w-full h-full pl-20 top-1/4 ml-10">
             <h1 className="font-Gilroy_sb text-blue text-7xl w-full">
               Welcome to <br/>   Literia
@@ -46,7 +46,7 @@ export default function landing(){
             </button>
           </div>
 
-          <div className="relative col-span-2 w-full h-full right-0">
+          <div className="relative col-span-1 w-full h-full right-0">
             <img
               className="object-cover w-full h-full"
               src="/image/logo_landing.png">
@@ -145,16 +145,65 @@ export default function landing(){
             </div>
           </div>
 
-          <div className="absolute w-2/3 h-3/4 left-0 right-0 top-0 bottom-0 items-center">
-            <div className="absolute w-[70px] h-[70px] right-0 left-0">
-              <img className="object-center w-full h-full"
-                   src="/image/logo_white.png">
-              </img>
+          <div className="absolute w-2/3 h-2/3 left-0 right-0 top-0 bottom-0 m-auto">
+            <div className="flex flex-col relative w-full h-full justify-items-center">
+            <div className="relative w-[60px] h-1/5 top-0 mx-auto">
+                <img className="object-contian w-full h-full"
+                    src="/image/logo_white.png">
+                </img>
+              </div>
+              <div className="relative w-auto h-1/6 right-0 left-0 top-2 mx-auto ">
+                <h2 className="font-Gilroy_sb text-white text-[30px]">
+                  Most growing platform
+                </h2>
+              </div>
+              <div className="relative w-auto h-1/4 right-0 left-0 mx-auto">
+                <h1 className="font-Gilroy_sb text-white text-[50px]">
+                  Literia's Creator
+                </h1>
+              </div>
+              <div className="relative w-3/4 h-1/5 right-0 left-0 mx-auto">
+                <p className="font-Gilroy_sb text-center text-white text-[22px]">
+                Access a world of knowledge and endless adventures at your fingertips with our library app.
+                </p>
+              </div>
+              <div className="relative w-auto h-1/4 right-0 left-0 top-5 m-auto">
+                <button className="w-auto h-auto px-8 py-1 button_red text-white text-[32px]">
+                  Join Now
+                </button>
+              </div>
             </div>
           </div>
         </div>
 
-        <div class="absolute w-screen h-[2000px] bg-yellow"></div>
+        <div className="grid grid-flow-col grid-cols-2 relative w-screen h-screen bg-white">
+          <div className="col-span-1 relative w-full h-full">
+            <div className="absolute w-full h-full">
+              <img className="object-cover w-full h-full "
+                   src="/image/blur_landing.png">
+              </img>            
+            </div> 
+            
+            <div className="absolute w-full h-full transform scale-75 -left-[100px]">
+              <img className="object-scale-down w-full h-full"
+                   src="/image/book_landing.png">
+              </img>
+            </div>
+          </div>
+          <div className="col-span-1 flex flex-col relative w-4/5 h-2/3 top-0 bottom-0 right-0 left-0 m-auto">
+              <h2 className="h-1/3 font-Gilroy_sb text-blue text-[50px] text-left top-0 bottom-0">
+                Newest book from <br/> your favorite author
+              </h2>
+              <p className="h-1/2 font-Gilrow_sv text-black text-lg text-left mt-5 top-0 bottom-0">
+              Stories have the incredible ability to transport you to distant lands, introduce you to fascinating characters, and ignite your emotions. Whether it's a classic novel, a captivating short story, or an engaging podcast, there's a story waiting to captivate your heart and mind. Dive in today and let the magic of storytelling transform your world.                
+              </p>
+              <button className="px-8 py-1 w-fit h-auto button_yellow text-blue text-lg left-0">
+                Join Now
+              </button>
+            </div>
+
+        </div>
+        {/* <div class="absolute w-screen h-[2000px] bg-yellow"></div> */}
       </div>
       
     </div>
