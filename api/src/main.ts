a83ac84aaeb5d4a7337284b3b4247bf443ea6bc0
Văn Phúc Lưu <94552405+hoktro@users.noreply.github.com>
2023-12-29 02:38:10 +0700

    Accept cors to port 3000

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,14 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Enable CORS
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3000', // Replace with your React app's URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  };
+  app.enableCors(corsOptions);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: false,
     }),
   );
+
   await app.listen(3030);
 }
+
 bootstrap();
