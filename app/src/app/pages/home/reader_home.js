f1340512c6cc11f27d0b7fa12f1a4e7674bf48fc
Văn Phúc Lưu <94552405+hoktro@users.noreply.github.com>
2023-12-29 23:37:45 +0700

    Test fetch and download pdf from mongo

--- app/src/app/pages/home/reader_home.js
+++ app/src/app/pages/home/reader_home.js
@@ -37,7 +37,7 @@ export default function ReaderHome() {
               <h3>{book.BDetail_title}</h3>
               <p style={{ color: 'black' }}>Genre: {book.BDetail_genre}</p>
               <p style={{ color: 'black' }}>Author: {book.BDetail_authorID}</p>
-              <p style={{ color: 'black' }}>Average Rating: {book.BDetail_averageRating}</p>
+              <p style={{ color: 'black' }}>Average Rating: {book.BDetail_averageRating !== undefined ? book.BDetail_averageRating : 0}</p>
               {book.BDetail_image && (
                 <img
                   src={`data:image/png;base64,${book.BDetail_image}`}
@@ -53,3 +53,50 @@ export default function ReaderHome() {
     </div>
   );
 }
+
+// const response = await fetch('http://localhost:3030/book/retrievePDF/658e8672fe9a28e26ec6be0d'); 
+
+// // download-pdf.js
+// import { useEffect } from 'react';
+// import axios from 'axios';
+
+// const ReaderHome = () => {
+//   const downloadPDF = async () => {
+//     try {
+//       // Fetch the Base64-encoded PDF from the server
+//       const response = await axios.get(
+//         'http://localhost:3030/book/retrievePDF/658e8672fe9a28e26ec6be0d', // Replace with the appropriate URL
+//       );
+
+//       // Decode the Base64 string to binary data
+//       const binaryData = atob(response.data);
+
+//       // Create a Uint8Array from the binary data
+//       const arrayBuffer = new Uint8Array(binaryData.length);
+//       for (let i = 0; i < binaryData.length; i++) {
+//         arrayBuffer[i] = binaryData.charCodeAt(i);
+//       }
+
+//       // Create a Blob from the binary data
+//       const blob = new Blob([arrayBuffer], { type: 'application/pdf' });
+
+//       // Create a link element and simulate a click to trigger the download
+//       const link = document.createElement('a');
+//       link.href = window.URL.createObjectURL(blob);
+//       link.download = 'your-file-name.pdf'; // Replace with the desired file name
+//       document.body.appendChild(link);
+//       link.click();
+//       document.body.removeChild(link);
+//     } catch (error) {
+//       console.error('Error downloading PDF:', error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     downloadPDF();
+//   }, []);
+
+//   return <div>Downloading PDF...</div>;
+// };
+
+// export default ReaderHome;
