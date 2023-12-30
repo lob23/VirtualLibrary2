d22e580b8878634e6639e34677bd2cdd136aa210
tttrinhtran <89054365+tttrinhtran@users.noreply.github.com>
2023-12-30 13:38:19 +0700

    conflict reader home

--- app/src/app/pages/home/reader_home.js
+++ app/src/app/pages/home/reader_home.js
@@ -1,6 +1,10 @@
 "use client";
 import { useEffect, useState } from "react";
 import { fetchData } from "../api/home/route";
+import _footer from "@/app/pages/wrapper/footer"
+
+const list = ['New', 'Money', 'Suit', 'And', 'Tie','New', 'Can', 'Read', 'Your', 'Mind', 'And', 'I', 'Know', 
+'Your', 'Story', 'Fuck', 'Love', 'Shiba']
 
 export default function ReaderHome() {
 
@@ -25,32 +29,85 @@ export default function ReaderHome() {
   }, []);
 
   return (
-    <div>
-      {loading ? (
-        // Loading state
-        <p>Loading...</p>
-      ) : (
-        // Display books
-        <ul>
-          {books.map((book) => (
-            <li key={book._id} style={{ color: 'black' }}>
-              <h3>{book.BDetail_title}</h3>
-              <p style={{ color: 'black' }}>Genre: {book.BDetail_genre}</p>
-              <p style={{ color: 'black' }}>Author: {book.BDetail_authorID}</p>
-              <p style={{ color: 'black' }}>Average Rating: {book.BDetail_averageRating !== undefined ? book.BDetail_averageRating : 0}</p>
-              {book.BDetail_image && (
-                <img
-                  src={`data:image/png;base64,${book.BDetail_image}`}
-                  alt={`Book cover for ${book.BDetail_title}`}
-                  style={{ maxWidth: '100%', maxHeight: '200px' }}
-                />
-              )}
-              {/* Add more details as needed */}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    // <div>
+    //   {loading ? (
+    //     // Loading state
+    //     <p>Loading...</p>
+    //   ) : (
+    //     // Display books
+    //     <ul>
+    //       {books.map((book) => (
+    //         <li key={book._id} style={{ color: 'black' }}>
+    //           <h3>{book.BDetail_title}</h3>
+    //           <p style={{ color: 'black' }}>Genre: {book.BDetail_genre}</p>
+    //           <p style={{ color: 'black' }}>Author: {book.BDetail_authorID}</p>
+    //           <p style={{ color: 'black' }}>Average Rating: {book.BDetail_averageRating}</p>
+    //           {book.BDetail_image && (
+    //             <img
+    //               src={`data:image/png;base64,${book.BDetail_image}`}
+    //               alt={`Book cover for ${book.BDetail_title}`}
+    //               style={{ maxWidth: '100%', maxHeight: '200px' }}
+    //             />
+    //           )}
+    //           {/* Add more details as needed */}
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   )}
+    // </div>
+    
+    
+
+
+    <div className="relative w-screen h-full">
+      <div className="relative w-full h-full overflow-y-auto no-scrollbar">
+        <div className="grid grid-flow-col grid-cols-2 relative w-screen h-screen bg-white">
+          <div className="relative col-span-1 flex flex-col flex-col-2 w-full h-auto pl-20 top-1/4 ml-10">
+            <h1 className="font-Gilroy_sb text-blue text-7xl w-full h-1/4">
+              New & <br/>   Trending
+            </h1>
+            <p className="font-Gilroy_md text-black text-md w-1/2 mt-3 text-[20px] leading-8">
+            Indulge in the latest and most captivating books, where every page invites you on a journey of imagination, intrigue, and discovery.
+            </p>
+          </div>
+          <div className="col-span-1 relative w-full h-full">
+            <div className="absolute w-full h-full">
+              <img className="object-cover w-full h-full "
+                    src="/image/blur.png">
+              </img>            
+            </div> 
+            
+            <div className="absolute w-full h-full left-0 right-0 top-0 bottom-0">
+              <img className="object-scale-down w-full h-full"
+                    src="/image/book_reader_home.png">
+              </img>
+            </div>
+          </div>
+        </div>
+        <div className="relative w-screen h-full">
+          <div className="relative w-full h-1/2 mx-[20px]">
+            <h2 className="font-Gilroy_sb text-blue text-3xl w-auto h-1/6">
+              Your reading list
+            </h2>
+            <ul className="relative flex flex-row gap-4 overflow-x-scroll no-scrollbar w-screen h-full py-10">
+              {
+                list.map((item)=>(
+                  <li className="relative w-full h-full">
+                    {<_footer/>}
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+
+        </div>
+
+        <div className="absolute w-screen h-[2000px] bg-yellow"></div>
+      </div>
+      
+  </div>
+
+
   );
 }
 
