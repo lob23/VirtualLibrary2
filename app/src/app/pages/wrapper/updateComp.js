f825d96d5722fea3fdf29c937cfcc26943a4a5f8
Nguyen Phuc Bao Uyen <hollyuyensan@gmail.com>
2024-01-06 22:47:39 +0700

    Update updateComp.js

--- app/src/app/pages/wrapper/updateComp.js
+++ app/src/app/pages/wrapper/updateComp.js
@@ -1,6 +1,8 @@
 "use client";
 import { useEffect, useState } from "react";
 import { fetchData } from "../api/wrapper/route";
+
+
 export default function updateComp(){
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -23,36 +25,25 @@ export default function updateComp(){
     fetchDataFromApi();
     
   }, []);
-    return(
-        
-        <>
+  return (
+    <>
+      {books.length > 0 && (
         <div className="group relative w-[275px] h-[375px] cursor-pointer overflow-hidden hover:shadow-cream/30 transition-shadow"> 
-            <img className="object-cover w-full h-full rounded-3xl transition-transform duration-500"
-                  src="/image/book.png"
-                >
-            </img>
-            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent group-hover:to-cream rounded-3xl">
-            </div>
-            <div className="absolute inset-0 flex flex-col mx-5 text-left translate-y-[20%] group-hover:translate-y-0 transition-all duration-500">
-                <div className="relative w-full h-full">
-                    <div className="absolute w-full h-1/3 bottom-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                    <h3 className="font-Gilroy_bd text-xl text-red ">
-                        {/* {books[0].BDetail_title} */}
-                    </h3>
-                    <h1 className="text-2xl font-Gilroy_sb text-blue">
-                        {/* {book.BDetail_title} */}
-                    </h1>
-                    <h2 className="text-lg font-Gilroy_md text-blue">
-                        {/* {book.BDetail_authorID} */}
-                        Author
-                    </h2>
-                </div>
-                </div>
-            </div>
+          {/* Your existing JSX code */}
+          <h3 className="font-Gilroy_bd text-xl text-red">
+            {books[0].BDetail_title}
+          </h3>
+          <h1 className="text-2xl font-Gilroy_sb text-blue">
+            {/* {book.BDetail_title} */}
+          </h1>
+          <h2 className="text-lg font-Gilroy_md text-blue">
+            {/* {book.BDetail_authorID} */}
+            Author
+          </h2>
         </div>
-        </>
-    ); 
-
+      )}
+    </>
+  );
     // return(
     //     <>
     //     <div className="relative w-0 h-full overflow-hidden">
