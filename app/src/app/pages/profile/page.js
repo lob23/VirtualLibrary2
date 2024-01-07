2888d21cf6abb0aeee792e2af2887e72405a998a
tttrinhtran <89054365+tttrinhtran@users.noreply.github.com>
2024-01-07 16:17:33 +0700

    profile create

---
+++ app/src/app/pages/profile/page.js
@@ -0,0 +1,10 @@
+import React from 'react';  // Import React
+import _profilePage from "@/app/pages/profile/profile_page" 
+
+export default function profile_screen(){
+    return(
+       <div>
+            <_profilePage/>
+       </div>
+    );
+}
---
+++ app/src/app/pages/profile/profile_page.js
@@ -0,0 +1,10 @@
+"use client";
+export default function profilePage(){
+    return(
+        <>
+        <div className="relative w-screen h-screen bg-blue">
+            
+        </div>
+        </>
+    ); 
+}
