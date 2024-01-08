0613538b380e908130a1b8881a469897a71ba874
Văn Phúc Lưu <94552405+hoktro@users.noreply.github.com>
2024-01-08 20:44:20 +0700

    Complete backend for notification

--- app/src/app/pages/api/wrapper/route.js
+++ app/src/app/pages/api/wrapper/route.js
@@ -26,3 +26,46 @@ export const fetchData = async () => {
     }
   };
   
+export const getNotifyList = async (uid) => {
+  const apiUrl = `http://localhost:3030/notification/getNotifyList/${uid}`; // replace with your actual API endpoint
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) throw new Error('Network response was not ok');
+
+    const data = await response.json();
+    return data;
+  } 
+  catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
+
+export const getNotify = async (notification_id) => {
+  const apiUrl = `http://localhost:3030/notification/getNotify/${notification_id}`; // replace with your actual API endpoint
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) throw new Error('Network response was not ok');
+
+    const data = await response.json();
+    return data;
+  } 
+  catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
--- app/src/app/pages/wrapper/Dropdown.js
+++ app/src/app/pages/wrapper/Dropdown.js
@@ -1,10 +1,35 @@
 "use client"
-import React from "react";
-import noti from "./noti.json";
+
 import _dropDrownComp from "./dropDownComp.js";
+import React, { useEffect } from "react";
 import { useState } from "react";
-const temp  = ['a', 'b','c', 'd', 'e']; 
+import { getNotifyList } from '@/app/pages/api/wrapper/route'
+import { useSearchParams } from "next/navigation";
+
 export default function Dropdown(){
+
+    const searchParams = useSearchParams();
+    const uid = searchParams.get('uid');
+
+    const [notifyList, setList] = useState();
+
+    useEffect( () => {
+
+        const prepare = async () => {
+            try {
+                // Get notification list of current user
+                const response = await getNotifyList(uid);
+                setList(response);
+                console.log('Notification list: ', notifyList);
+            }
+            catch (error) {
+                console.error('Error: ', error);
+            }
+        }
+
+        prepare()
+    }, []);
+
     return(
         <>
         <div className="relative w-[400px] h-[400px] bg-white rounded-2xl overflow-y-auto flex flex-col shadow-lg">
@@ -17,10 +42,9 @@ export default function Dropdown(){
             </div>
             <ul className="relative flex flex-col overflow-y-auto no-scrollbar w-full h-auto list-none">
                 {
-                    noti.map((item) => (
-                        <li className="relative w-full h-full">
-                            {/* <_dropDrownComp/> */}
-                            {_dropDrownComp(item)}
+                    notifyList && notifyList.map((item) => (
+                        <li className="relative w-full h-full" key={item.id}>
+                          {_dropDrownComp(item)}
                         </li>
                     ))
                 }
