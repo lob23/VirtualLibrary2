1bafbd095018a75fd3b9fb2b6e4c1ea462eede42
Gia-Thinh Pham <thinhcm2003@gmail.com>
2024-01-08 17:06:28 +0900

    search form update

--- app/src/app/pages/search/search_form.js
+++ app/src/app/pages/search/search_form.js
@@ -1,80 +1,71 @@
-"use client"; // This is a client component 👈🏽
+"use client"; // This is a client component
 
 import { useState } from "react";
-import {Link} from "react-scroll"
+import { Link } from "react-scroll"
 import _footer from "@/app/pages/wrapper/footer"
 
 export default function search_form() {
   const [searchResults, setSearchResults] = useState([]);
 
-  const handleSearch = (searchTerm) => {
-    // Perform your search logic here and update searchResults state
-    // For simplicity, let's just log the search term for now
-    console.log("Search term:", searchTerm);
-  };
 
   return (
     <>
-    <div class="relative w-screen h-auto bg-cream">
-        <h1 className="static font-Gilroy_md text-blue text-4xl text-center pt-40 mb-30 pb-5 z-15">
-          Choose your stories
-        </h1>
-        <div class="z-5">
+      <div className=" flex flex-col w-full h-auto  ">
+        <div class="relative w-screen h-[300px] bg-cream ">
+          <h1 className="static font-Gilroy_md text-blue text-4xl text-center pt-40 mb-30 pb-5 z-15">
+            Choose your stories
+          </h1>
+          <div class="z-5">
             <div className="absolute -bottom-20 -right-20 w-[200px] h-[200px] transform rotate-90">
-              <img className="object-center  w-full h-full " 
-                    src="/image/reg_cir.png">
+              <img className="object-center  w-full h-full "
+                src="/image/reg_cir.png">
               </img>
-            </div>  
+            </div>
 
             <div className="absolute -bottom-25 -left-40 w-[300px] h-[300px] transform -rotate-90">
               <img className="object-center w-full h-full "
-                   src="/image/reg_cir.png">
+                src="/image/reg_cir.png">
               </img>
             </div>
 
             <div className="absolute -bottom-20 left-10 w-40 h-40 transform -rotate-120">
               <img className="object-center w-full h-full"
-                    src="/image/reg_cir.png"/>
+                src="/image/reg_cir.png" />
             </div>
+          </div>
         </div>
-
-        <div class="relative z-40 mx-60 mt-20 flex items-center -bottom-7 h-14 rounded-lg bg-white overflow-hidden shadow-md shadow-blue-500/50">
-          <div class="grid place-items-center h-full w-10 mx-2 text-gray-300 p-2 relative">
+        <div className=" absolute transform translate-y-[270px] translate-x-1/2 flex flex-row w-1/2  h-[60px] rounded-[12px]  overflow-hidden bg-white items-center drop-shadow-xl ">
+          <div className="ml-4">
             <svg width="30" height="30" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
               <g id="Group 1937">
-                <path id="Stroke 455" d="M1.99965 35.854L11.0273 26.8263" stroke="#27219A" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-                <path id="Stroke 456" fill-rule="evenodd" clip-rule="evenodd" d="M21.3447 2C13.331 2 6.83593 8.4951 6.83593 16.5088C6.83593 24.5225 13.331 31.0176 21.3447 31.0176C29.3584 31.0176 35.8535 24.5225 35.8535 16.5088C35.8535 8.4951 29.3584 2 21.3447 2V2Z" stroke="#27219A" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+                <path id="Stroke 455" d="M1.99965 35.854L11.0273 26.8263" stroke="#27219A" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                <path id="Stroke 456" fill-rule="evenodd" clip-rule="evenodd" d="M21.3447 2C13.331 2 6.83593 8.4951 6.83593 16.5088C6.83593 24.5225 13.331 31.0176 21.3447 31.0176C29.3584 31.0176 35.8535 24.5225 35.8535 16.5088C35.8535 8.4951 29.3584 2 21.3447 2V2Z" stroke="#27219A" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
               </g>
             </svg>
           </div>
 
           <input
-            class="h-full w-full text-lg text-gray-700 border-0 focus:border-0 shadow-none py-20 placeholder:text-blue placeholder:text-opacity-50 placeholder:font-Gilroy_md relative"
+            className="h-full w-4/5 text-lg text-gray-700 border-0 focus:border-0 shadow-none  placeholder-blue/40 placeholder:pl-4 font-Gilroy_md bg-white"
             type="text"
             id="search"
             placeholder="Enter the story or the author"
           />
           <button
-            class="relative w-[150px] h-full my-5 bg-[#27219A] p-3 border-none shadow-none text-yellow text-xl font-Gilroy_md hover:bg-[#19156e]"
+            className="w-1/5 h-full bg-blue p-3 border-none shadow-none text-yellow text-xl font-Gilroy_md hover:bg-[#19156e]"
             type="submit"
           >
             Search
           </button>
         </div>
-    </div>
-     
-    <div className="relative w-screen h-auto bg-white">
-      {/* Latest update section on the left  */}
-        <div className="relative w-screen h-auto left-10 pt-10">
-          <h1 className="static font-Gilroy_md text-blue text-3xl ">
-            Latest Update
-          </h1>
-          <div className="relative w-screen h-auto">
-          </div>
-        </div>
-    </div>
-    
-    
+
+      </div>
+
+
+
+
+
+
+
     </>
   );
 }
@@ -82,7 +73,7 @@ export default function search_form() {
 
 
 
- {/* <div class="relative w-screen h-full bg-white">
+{/* <div class="relative w-screen h-full bg-white">
         <div class="relative w-screen h-auto bg-cream z-0">
           <h1 className="static font-Gilroy_md text-blue text-4xl text-center mt-40 mb-30 pb-5">
           Choose your stories
