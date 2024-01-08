7e54e2d6d065eb03cce01a657e47b51c622874f7
Nguyen Phuc Bao Uyen <hollyuyensan@gmail.com>
2024-01-08 15:40:37 +0700

    Update pdfReader.js

--- app/src/app/pages/reading/pdfReader.js
+++ app/src/app/pages/reading/pdfReader.js
@@ -19,20 +19,20 @@ import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import { Audio } from "react-loader-spinner";
 
 const base64toBlob = (data) => {
-        const pdfContentType = 'application/pdf';
+    const pdfContentType = 'application/pdf';
 
-        // Cut the prefix `data:application/pdf;base64` from the raw base 64
-        const base64WithoutPrefix = data.substr(`data:${pdfContentType};base64,`.length);
+    // Cut the prefix `data:application/pdf;base64` from the raw base 64
+    const base64WithoutPrefix = data.substr(`data:${pdfContentType};base64,`.length);
 
-        const bytes = atob(base64WithoutPrefix);
-        let length = bytes.length;
-        let out = new Uint8Array(length);
+    const bytes = atob(base64WithoutPrefix);
+    let length = bytes.length;
+    let out = new Uint8Array(length);
 
-        while (length--) {
-            out[length] = bytes.charCodeAt(length);
-        }
+    while (length--) {
+        out[length] = bytes.charCodeAt(length);
+    }
 
-        return new Blob([out], { type: pdfContentType });
+    return new Blob([out], { type: pdfContentType });
 };
 
 export default function _readingpage() {
@@ -43,60 +43,59 @@ export default function _readingpage() {
 
     const uid = searchParams.get("uid");
     const bDetailID = searchParams.get("bid");
-    
+
     useEffect(() => {
         const fetchingBookContents = async () => {
-            const res = await fetch("api/bookcontent?bid="+bDetailID,{
+            const res = await fetch("api/bookcontent?bid=" + bDetailID, {
                 method: "GET",
             });
             const result = await res.json();
-            if (result.stat == true){
-                console.log("book content: ",result.bookContent.BContent_pdf)
+            if (result.stat == true) {
+                console.log("book content: ", result.bookContent.BContent_pdf)
                 setBase64String("data:application/pdf;base64," + result.bookContent.BContent_pdf);
-            } else{
+            } else {
                 console.log("error", result.error)
             }
         }
         fetchingBookContents()
-    },[])
-    useEffect( () => {
-        const blob =   base64toBlob(base64String);
-        const turl =   URL.createObjectURL(blob);
+    }, [])
+    useEffect(() => {
+        const blob = base64toBlob(base64String);
+        const turl = URL.createObjectURL(blob);
         seturl(turl)
-      }, [base64String]);
+    }, [base64String]);
 
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
     return (
-        <>  
-            {base64String?
-                <div className='h-screen w-screen flex items-center overflow-y-hidden'>
-                    <div className='h-1/5'>
-                            <img
-                            className="object-contain row-span-1 col-span-5"
+        <>
+            {base64String ?
+                <div className='h-screen w-screen flex flex-col items-center overflow-y-hidden'>
+                    <div className='h-fit'>
+                        <img
+                            className="object-contain"
                             src="/image/logo.png">
-                            </img>
+                        </img>
                     </div>
                     <div className='h-4/5 w-4/5'>
-
-                            <Viewer fileUrl={_url} plugins={[
-                                // Register plugins
-                                defaultLayoutPluginInstance
-                            ]} />
+                        <Viewer fileUrl={_url} plugins={[
+                            // Register plugins
+                            defaultLayoutPluginInstance
+                        ]} />
                     </div>
                 </div>
-            :
-                <div className='flex items-center justify-center h-screen'>
+                :
+                <div className='flex items-center justify-center h-screen '>
                     <Audio
-                    height="80"
-                    width="80"
-                    radius="9"
-                    color="green"
-                    ariaLabel="loading"
+                        height="80"
+                        width="80"
+                        radius="9"
+                        color="blue"
+                        ariaLabel="loading"
                     />
                 </div>
             }
-            
+
         </>
 
     )
