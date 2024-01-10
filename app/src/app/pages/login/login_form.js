474f8f4184320ebe4b580712ec773c3b61b7ade1
Nguyen Phuc Bao Uyen <hollyuyensan@gmail.com>
2024-01-10 17:53:15 +0700

    Login

--- app/src/app/pages/login/login_form.js
+++ app/src/app/pages/login/login_form.js
@@ -24,19 +24,19 @@ export default function login_form() {
         password,
       }),
     });
-    const user = await res.json().then(result => { return result})
+    const user = await res.json().then(result => { return result })
     // console.log("Userrr: ", user)
     // console.log("Userrr: ", user.User_name)
 
     // console.log("Userrr: ", user.User_email)
-    if (user == null){
+    if (user == null) {
       toast.error("Try Again !", {
         position: toast.POSITION.TOP_CENTER,
         autoClose: 3000
       });
 
     } else {
-      if (user){
+      if (user) {
         if (user.User_authenticationLevel == 1)
           router.push("/pages/homeReader?uid=" + user._id);
         else if (user.User_authenticationLevel == 2)
@@ -50,87 +50,131 @@ export default function login_form() {
   return (
     <>
       <div className="relative w-screen h-screen bg-registerbg bg-fixed overflow-hidden">
-        
-        <div className="absolute  -bottom-20 -right-20  w-[300px] h-[300px]">
-          <img
-            className="object-contain w-full h-full  "
-            src="/image/reg_cir.png"
-          ></img>
-        </div>
-        <div>
-          <ToastContainer/>
-        </div>
-        <div className="absolute bottom-[20px] left-[120px]  w-[120px] h-[150px]">
-          <img
-            className="object-contain w-full h-full "
-            src="/image/reg_cir.png"
-          ></img>
-        </div>
-        <div className="absolute top-[60px] right-[120px]  w-[80px] h-[80px]">
-          <img
-            className="object-contain w-full h-full "
-            src="/image/reg_cir.png"
-          ></img>
-        </div>
-        <div className="grid grid-flow-col grid-cols-3 grid-rows-none absolute top-0 left-0 right-0 bottom-0  w-2/3 h-3/4 m-auto rounded-[40px] bg-white/70 backdrop-blur-sm overflow-hidden">
-          <div className="col-span-1 flex flex-col px-10 py-20 w-auto h-full bg-white">
+        <div className="w-screen h-screen justify-center items-center ">
+          <div className="absolute  -bottom-20 -right-20  w-[300px] h-[300px]">
             <img
-              className="object-scale-down w-full h-1/2 "
-              src="/image/reg_img1.png"
+              className="object-contain w-full h-full  "
+              src="/image/reg_cir.png"
             ></img>
-            <h1 className="font-Gilroy_bd text-blue text-[40px] mt-[10px] leading-10">
-              Welcome to<br></br> Literia{" "}
-            </h1>
-            <h3 className="font-Gilroy_sb text-blue text-[20px] mt-[10px]">
-              Hundred of book to choose
-            </h3>
           </div>
-          <div className="col-span-2 flex flex-col  justify-center items-center w-auto sc-full ">
-            <h1 className="font-Gilroy_bd text-blue text-[40px] -ml-[260px]">
-              Sign In
-            </h1>
-            <div className="flex flex-col w-max h-max">
-              <div>
-                <form
-                  onSubmit={handleSubmit}
-                  className="grid grid-cols-none grid-row-4"
-                >
-                  <input
-                    type="text"
-                    onChange={(e) => setUsername(e.target.value)}
-                    value={username}
-                    id="username"
-                    placeholder="Username"
-                    className="row-span-1 w-[380px] h-[38px] my-2   "
-                  />
-                  <input
-                    type="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    id="password"
-                    placeholder="Password"
-                    className="row-span-1 w-[380px] h-[38px] my-2 "
-                  />
-                  <p className="items-end ml-auto font-Gilroy_md text-red text-sm">
-                    Forgot password?
-                  </p>
-                  <div className="flex flex-row items-center justify-center">
-                   <label className=" w-[380px] h-[60px] my-5 bg-blue p-3 rounded-[12px] border-none text-white text-xl font-Gilroy_bd   items-center justify-center text-center"> 
-                    <button
-                      type="submit"
-                    >
-                      Sign In
-                    </button>
-                    
-                  </label>
-                  
-                  </div>
-                </form>
+          <div>
+            <ToastContainer />
+          </div>
+          <div className="absolute bottom-[20px] left-[120px]  w-[120px] h-[150px]">
+            <img
+              className="object-contain w-full h-full "
+              src="/image/reg_cir.png"
+            ></img>
+          </div>
+          <div className="absolute top-[60px] right-[120px]  w-[80px] h-[80px]">
+            <img
+              className="object-contain w-full h-full "
+              src="/image/reg_cir.png"
+            ></img>
+          </div>
+          <div className="flex flex-wrap w-full h-full justify-center items-center">
+            <div className="flex flex-row w-3/4 h-3/4 bg-white/70 backdrop-blur-sm overflow-hidden rounded-[40px] ">
+              <div className="flex flex-col w-1/3 h-full bg-white justify-center  px-5">
+                <img
+                  className="object-scale-down w-full h-1/2  "
+                  src="/image/reg_img1.png"
+                ></img>
+                <h1 className="font-Gilroy_bd text-blue text-[40px] mt-[10px] leading-10">
+                  Welcome to<br></br> Literia{" "}
+                </h1>
+                <h3 className="font-Gilroy_sb text-blue text-[20px] mt-[10px]">
+                  Hundred of book to choose
+                </h3>
               </div>
+
+              <div className="flex flex-col  justify-center items-center w-2/3 ">
+                <h1 className="font-Gilroy_bd text-blue text-[40px] -ml-[260px]">
+                  Sign In
+                </h1>
+                <div className="flex flex-col w-max h-max">
+                  <div>
+                    <form
+                      onSubmit={handleSubmit}
+                      className="grid grid-cols-none grid-row-4"
+                    >
+                      <input
+                        type="text"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={username}
+                        id="username"
+                        placeholder="Username"
+                        className="row-span-1 w-[380px] h-[38px] my-2   "
+                      />
+                      <input
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        id="password"
+                        placeholder="Password"
+                        className="row-span-1 w-[380px] h-[38px] my-2 "
+                      />
+                      <p className="items-end ml-auto font-Gilroy_md text-red text-sm">
+                        Forgot password?
+                      </p>
+                      <div className="flex flex-row items-center justify-center">
+
+                        <button
+                          type="submit"
+                          className=" w-[380px] h-[60px] my-5 bg-blue p-3 rounded-[12px] border-none text-white text-xl font-Gilroy_bd   items-center justify-center text-center"
+                        >
+                          Sign In
+                        </button>
+
+
+
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+
             </div>
+
           </div>
+
+
+
         </div>
+
       </div>
     </>
   );
 }
+
+
+{/* <div className="col-span-2 flex flex-col  justify-center items-center w-auto h-full ">
+  <h1 className="font-Gilroy_bd text-blue text-[40px] -ml-[250px]">
+    {isSetAccount ? "Sign up" : "Personal Information"}
+  </h1>
+  <div className="flex flex-col w-max h-max">
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        onReset={handleSubmit}
+        className="grid grid-cols-none grid-row-4"
+      >
+        {!isSetAccount ? personalInformationFragment : accountFragemnt}
+
+        <p className="items-end ml-auto font-Gilroy_md text-red text-sm">
+          Already have an account? Sign in
+        </p>
+        <label className=" w-[380px] h-[60px] my-5 bg-blue p-3 rounded-[12px] border-none text-white text-xl font-Gilroy_bd text-center"
+        >
+          <button
+
+            type='reset'
+          >
+            Sign Up
+          </button>
+
+        </label>
+
+      </form>
+    </div>
+  </div>
+</div> */}
--- app/src/app/pages/signup/signup_form.js
+++ app/src/app/pages/signup/signup_form.js
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import {DatePicker} from "antd"
+import { DatePicker } from "antd"
 import dayjs from "dayjs";
 import { useRouter } from "next/navigation";
 
@@ -21,32 +21,32 @@ export default function signup_form() {
   const router = useRouter()
 
   const accountFragemnt = (
-  <>
-    <input
-      type="text"
-      onChange={(e) => setUsername(e.target.value)}
-      value={username}
-      id="username"
-      placeholder="Email"
-      className="row-span-1 w-[380px] h-[38px] my-2   "
-    />
-    <input
-      type="password"
-      onChange={(e) => setPassword(e.target.value)}
-      value={password}
-      id="password"
-      placeholder="Password"
-      className="row-span-1 w-[380px] h-[38px] my-2  "
-    />
-    <input
-      type="password"
-      onChange={(e) => repeatPassword(e.target.value)}
-      value={repeatpassword}
-      id="repeatpassword"
-      placeholder="Confirm Password"
-      className="row-span-1 w-[380px] h-[38px] my-2  "
-    />
-  </>
+    <>
+      <input
+        type="text"
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+        id="username"
+        placeholder="Email"
+        className="row-span-1 w-[380px] h-[38px] my-2   "
+      />
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        id="password"
+        placeholder="Password"
+        className="row-span-1 w-[380px] h-[38px] my-2  "
+      />
+      <input
+        type="password"
+        onChange={(e) => repeatPassword(e.target.value)}
+        value={repeatpassword}
+        id="repeatpassword"
+        placeholder="Confirm Password"
+        className="row-span-1 w-[380px] h-[38px] my-2  "
+      />
+    </>
   )
 
 
@@ -86,7 +86,7 @@ export default function signup_form() {
       />
       <div >
         <DatePicker
-          placeholder={!date.value? "Date of Birth" : date }
+          placeholder={!date.value ? "Date of Birth" : date}
           className="row-span-1 w-[380px] h-[38px] my-2"
           onChange={(e) => {
             setNewDate(e.toDate());
@@ -99,8 +99,8 @@ export default function signup_form() {
 
 
   const handleSubmit = async (e) => {
-    if (isSetAccount == true){
-      if(username && password && repeatpassword && userfirstname && userlastname && userphone && useraddress && date){
+    if (isSetAccount == true) {
+      if (username && password && repeatpassword && userfirstname && userlastname && userphone && useraddress && date) {
         e.preventDefault();
 
         const res = await fetch("api/signup", {
@@ -114,27 +114,27 @@ export default function signup_form() {
             User_firstname: userfirstname,
             User_lastname: userlastname,
             User_phone: userphone,
-            User_address: useraddress ,
+            User_address: useraddress,
             User_dob: date,
           }),
         });
-        const user = await res.json().then(result => { return result})
+        const user = await res.json().then(result => { return result })
         console.log("res", user)
         if (user) router.push("login")
         else {
-          toast.error("The account is existed",{
+          toast.error("The account is existed", {
             position: toast.POSITION.TOP_CENTER,
             autoClose: 3000
           });
         }
       } else {
         toast.error("Please full fill the options !", {
-        position: toast.POSITION.TOP_CENTER,
-        autoClose: 3000
+          position: toast.POSITION.TOP_CENTER,
+          autoClose: 3000
         });
       }
     } else {
-      if (userfirstname && userlastname && userphone && useraddress && date){
+      if (userfirstname && userlastname && userphone && useraddress && date) {
         setAccount(true)
       } else {
         toast.error("Please full fill the options !", {
@@ -148,73 +148,85 @@ export default function signup_form() {
   return (
     <>
       <div className="relative w-screen h-screen bg-registerbg bg-fixed overflow-hidden">
-        <div className="absolute  -bottom-20 -right-20  w-[300px] h-[300px]">
-          <img
-            className="object-contain w-full h-full  "
-            src="/image/reg_cir.png"
-          ></img>
-        </div>
-        <div>
-          <ToastContainer/>
-        </div>
-        <div className="absolute bottom-[20px] left-[120px]  w-[120px] h-[150px]">
-          <img
-            className="object-contain w-full h-full "
-            src="/image/reg_cir.png"
-          ></img>
-        </div>
-        <div className="absolute top-[60px] right-[120px]  w-[80px] h-[80px]">
-          <img
-            className="object-contain w-full h-full "
-            src="/image/reg_cir.png"
-          ></img>
-        </div>
-        <div className="grid grid-flow-col grid-cols-3 grid-rows-none absolute top-0 left-0 right-0 bottom-0  w-2/3 h-3/4 m-auto rounded-[40px] bg-white/70 backdrop-blur-sm overflow-hidden">
-          <div className=" col-span-1 flex flex-col px-10 py-20 w-auto h-full bg-white">
+        <div className="w-screen h-screen justify-center items-center ">
+          <div className="absolute  -bottom-20 -right-20  w-[300px] h-[300px]">
             <img
-              className="object-scale-down w-full h-1/2 "
-              src="/image/reg_img1.png"
+              className="object-contain w-full h-full  "
+              src="/image/reg_cir.png"
             ></img>
-            <h1 className="  mt-[10px] font-Gilroy_bd text-blue text-[40px] leading-10">
-              Welcome to<br></br> Literia{" "}
-            </h1>
-            <h3 className="  mt-[10px] font-Gilroy_sb text-blue text-[20px]">
-              Hundred of book to choose
-            </h3>
           </div>
-          <div className="col-span-2 flex flex-col  justify-center items-center w-auto h-full ">
-            <h1 className="font-Gilroy_bd text-blue text-[40px] -ml-[250px]">
-              {isSetAccount ? "Sign up":"Personal Information"}
-            </h1>
-            <div className="flex flex-col w-max h-max">
-              <div>
-                <form
-                  onSubmit={handleSubmit}
-                  onReset={handleSubmit}
-                  className="grid grid-cols-none grid-row-4"
-                >
-                  {!isSetAccount ? personalInformationFragment : accountFragemnt}
-
-                  <p className="items-end ml-auto font-Gilroy_md text-red text-sm">
-                    Already have an account? Sign in
-                  </p>
-                  <label  className=" w-[380px] h-[60px] my-5 bg-blue p-3 rounded-[12px] border-none text-white text-xl font-Gilroy_bd text-center"
-                  >
-                  <button
-                   
-                    type='reset'
-                  >
-                    Sign Up
-                  </button>
-
-                  </label>
-                  
-                </form>
+          <div>
+            <ToastContainer />
+          </div>
+          <div className="absolute bottom-[20px] left-[120px]  w-[120px] h-[150px]">
+            <img
+              className="object-contain w-full h-full "
+              src="/image/reg_cir.png"
+            ></img>
+          </div>
+          <div className="absolute top-[60px] right-[120px]  w-[80px] h-[80px]">
+            <img
+              className="object-contain w-full h-full "
+              src="/image/reg_cir.png"
+            ></img>
+          </div>
+          <div className="flex flex-wrap w-full h-full justify-center items-center">
+            <div className="flex flex-row w-3/4 h-3/4 bg-white/70 backdrop-blur-sm overflow-hidden rounded-[40px] ">
+              <div className="flex flex-col w-1/3 h-full bg-white justify-center  px-5">
+                <img
+                  className="object-scale-down w-full h-1/2  "
+                  src="/image/reg_img1.png"
+                ></img>
+                <h1 className="font-Gilroy_bd text-blue text-[40px] mt-[10px] leading-10">
+                  Welcome to<br></br> Literia{" "}
+                </h1>
+                <h3 className="font-Gilroy_sb text-blue text-[20px] mt-[10px]">
+                  Hundred of book to choose
+                </h3>
               </div>
+
+              <div className="flex flex-col  justify-center items-center w-2/3 h-full ">
+                <h1 className="font-Gilroy_bd text-blue text-[40px] ">
+                  {isSetAccount ? "Sign up" : "Personal Information"}
+                </h1>
+                <div className="flex flex-col w-max h-max">
+                  <div>
+                    <form
+                      onSubmit={handleSubmit}
+                      onReset={handleSubmit}
+                      className="grid grid-cols-none grid-row-4"
+                    >
+                      {!isSetAccount ? personalInformationFragment : accountFragemnt}
+
+                      <p className="items-end ml-auto font-Gilroy_md text-red text-sm">
+                        Already have an account? Sign in
+                      </p>
+
+                      <button
+
+                        type='reset'
+                        className=" w-[380px] h-[60px] my-5 bg-blue p-3 rounded-[12px] border-none text-white text-xl font-Gilroy_bd text-center"
+                      >
+                        Sign Up
+                      </button>
+
+
+
+                    </form>
+                  </div>
+                </div>
+              </div>
+
             </div>
+
           </div>
+
+
+
         </div>
       </div>
+
+
     </>
   );
 }
