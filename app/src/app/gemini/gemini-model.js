dced1543b78987fbe9287572d5c832fd093dc5ce
minhvip08 <minhvip08@gmail.com>
2024-05-19 17:44:11 +0700

    model gemini

--- app/src/app/gemini/gemini-model.js
+++ app/src/app/gemini/gemini-model.js
@@ -18,7 +18,7 @@ async function gemini(prompt) {
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
-    console.log(text);
+    return text;
 }
 
 module.exports = gemini;
--- app/src/app/gemini/gemini-start.js
+++ app/src/app/gemini/gemini-start.js
@@ -1,3 +1,5 @@
 const gemini = require('./gemini-model');
 
-gemini("Write a sonnet about a programmers life, but also make it rhyme.");
+gemini("Write a sonnet about a programmers life, but also make it rhyme.").then((response) => {
+    console.log(response);
+});
